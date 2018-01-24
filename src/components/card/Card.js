19fab50f29e17c2eb83b4c9b003cfaa7ba50fb09
Michelle Hoffman <hoffman.michelle.e@gmail.com>
2018-01-23 18:22:42 -0700

    Add className to list items that have a data value below 0.5 percent

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -7,9 +7,15 @@ const Card = ({location, data}) => {
       <h2>{location}</h2>
       <ul>
       {
-        Object.keys(data).map(
-          year => <li> {`${year} : ${data[year]}`} </li>
-        )
+        Object.keys(data).map(year => {
+          let className;
+
+          if(data[year] < 0.5){
+            className = "under-fifty"
+          }
+
+          return <li className={className}>{year}: {data[year]}</li>
+        })
       }
       </ul>
     </div>
