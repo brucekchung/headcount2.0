e621e0b426ce71975eef2be15b617ebeac981520
Bruce Chung <brucekchung@gmail.com>
2018-01-24 10:58:54 -0700

    Begin app test

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,8 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import CardContainer from '../card-container/CardContainer';
+import Input from '../input/Input';
+import {shallow} from 'enzyme'
+import DistrictRepository from '../../helper.js';
+import data from '../../data/kindergartners_in_full_day_program.js';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  // ReactDOM.render(<App />, div);
-});
+import {configure} from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+configure({ adapter: new Adapter() })
+
+describe('App', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<App />)
+  })
+
+  it('should exist', () => {
+    expect(wrapper).toBeDefined()
+  })
+
+  it('should have a wrapper div', () => {
+    expect(wrapper.find('div')).toHaveLength(1)
+  })
+
+  it('should match the snapshot', () => {
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('should contain in Input and CardContainer component', () => {
+    expect(wrapper.find(<Input />)).toBeDefined()
+    expect(wrapper.find(<CardContainer />)).toBeDefined()
+  })
+
+  it('should load data into state', () => {
+    const district = new DistrictRepository(data);
+    wrapper.instance().handleSearch()
+    wrapper.update()
+    console.log('state: ', wrapper.state)
+    expect(wrapper.state).toHaveLength(181)
+  })
+})
+
+
+
