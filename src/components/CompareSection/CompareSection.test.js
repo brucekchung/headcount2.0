193d1e4709aacff537d0f82547fa94a0c052df20
Michelle Hoffman <hoffman.michelle.e@gmail.com>
2018-01-27 21:23:49 -0700

    Add tests for CompareSection that check what is rendered

--- src/components/CompareSection/CompareSection.test.js
+++ src/components/CompareSection/CompareSection.test.js
@@ -12,21 +12,20 @@ describe('CompareSection', () => {
   let wrapper
   const district = new DistrictRepository(kinderData)
   const calculateAverage = (a, b) => district.compareDistrictAverages(a.location, b.location)
-  const compare = [  {
+  const compare = [{
     "location": "ColoraDo",
-    "TimeFrame": 2014,
-    "DataFormat": "Percent",
-    "Data": 0.74118
+    "data": {2004: 0.756, 2005: 0.658, 2006: 0.253},
+    "id": 1
   },
   {
     "location": "ACADEMY 20",
-    "TimeFrame": 2007,
-    "DataFormat": "Percent",
-    "Data": 0.39159
+    "data": {2004: 0.465, 2005: 0.758, 2006: 0.856},
+    "id": 2
   }]
 
   beforeEach(() => {
-    wrapper = shallow(<CompareSection data={compare} calculateAverage={calculateAverage} />)
+    wrapper = shallow(<CompareSection data={compare}
+                                      calculateAverage={calculateAverage} />)
   })
 
   it('should exist', () => {
@@ -36,4 +35,26 @@ describe('CompareSection', () => {
   it('should match the snapshot', () => {
     expect(wrapper).toMatchSnapshot()
   })
+
+  it('should render two district cards and a comparison card', () => {
+    expect(wrapper.find('Card').length).toEqual(2)
+    expect(wrapper.find('.center').length).toEqual(1)
+  })
+
+  it('should display the average of both districts', () => {
+    expect(wrapper.find('h2').text()).toEqual('1.3');
+  })
+
+  it('should render a single card if there is only one selected', () => {
+    const selected=[{
+      "location": "ColoraDo",
+      "data": {2004: 0.756, 2005: 0.658, 2006: 0.253},
+      "id": 1
+    }]
+    wrapper = shallow(<CompareSection data={selected}
+                                      calculateAverage={calculateAverage} />)
+
+    expect(wrapper.find('Card').length).toEqual(1)
+  })
+
 })
